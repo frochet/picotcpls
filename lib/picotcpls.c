933cc3999e98cab67b679dead5578a806632e63d
Florentin Rochet <florentin.rochet@uclouvain.be>
2020-06-10 19:58:50 +0200

    cleanup printf

--- lib/picotcpls.c
+++ lib/picotcpls.c
@@ -304,13 +304,11 @@ int tcpls_connect(ptls_t *tls, struct sockaddr *src, struct sockaddr *dest,
       while (current_v4 || current_v6) {
         if (ours_current_v4 && current_v4) {
           if (handle_connect(tcpls, ours_current_v4, current_v4, NULL, NULL, AF_INET, &nfds, &maxfds, &coninfo, &wset) < 0) {
-	    printf("v4\n");
             return -1;
           }
         }
         if (ours_current_v6 && current_v6) {
           if(handle_connect(tcpls, NULL, NULL, ours_current_v6, current_v6, AF_INET6, &nfds, &maxfds, &coninfo, &wset) < 0) {
-            printf("v6\n");
             return -1;
           }
         }
@@ -1306,7 +1304,6 @@ Exit:
 }
 
 static int setlocal_usertimeout(ptls_t *ptls, int val) {
-  printf("should set  local user time out %d %d\n", val, ptls->tcpls->socket_primary);
   struct timeval timeout;      
   timeout.tv_sec = val;
   timeout.tv_usec = 0;
@@ -1551,7 +1548,6 @@ static void _set_primary(tcpls_t *tcpls) {
   }
   if (has_primary) {
     tcpls->socket_primary = primary_con->socket;
-    printf("primary 1 %d\n", tcpls->socket_primary);
     return;
   }
  
